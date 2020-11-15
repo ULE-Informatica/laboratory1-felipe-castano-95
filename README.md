eb8f9faeb3b1d01dd7760b89f9f51cbe1f965711
Felipe <lcastl02@gmail.com>
2020-11-16 00:27:55 +0100

    Adding source code and first table attemp

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dps-lab-1
 Laboratory 1
+
+1. Enumerate those Rules and Recommendation associated that are broken in the previous source code file.
+2. Fix any kind of error or mistake. Add a comment explaining your approach for solving it.
+
+Original position | Position after comments and changes | Error/Rule/Recommendation | Comments 
+--- | --- | --- | --- 
+18 | 11 | STR11-C: A char array does not need a null-terminated byte. |  Null-terminated byte was eliminated
+ --- | --- | --- | --- 
+
+
+
+
+
+
+
+
--- exampleStrings.c
+++ exampleStrings.c
@@ -1,54 +1,65 @@
-/**
- *  
- * The student needs to compile, test and comment the source code file.
- * 
- * Enumerate those Rules and Recommendation associated that are broken in the previous source code file.
- * Enumerate he compilation tools and paramenters (gcc vs g++), -Wall ...
- * Enumerate the standard associated  -std=c99, -std=c11
- * 
- * There are several variants. You should choose at least two.
- * At the end the source code  should compile without warnings to the variant selected (you can remove/change instructions).
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
+#include <stddef.h>
+#include <wchar.h>
+
 char array1[] = "Foo" "bar";
-char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
+/**
+* STR11-C: A char array does not need a null-terminated byte. Null-terminated byte was eliminated
+*/
+char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r' };
  
 enum { BUFFER_MAX_SIZE = 1024 };
- 
-const char* s1 = R"foo(
-Hello
-World
-)foo";
+/*
+* Error: C does not support Raw Strings Literals
+*/
+const char* s1 = "\nHello\nWorld\n";
 const char* s2 = "\nHello\nWorld\n";
 
+/*
+* Waring: ‘return’ with a value, in function returning void 
+* Changed == NULL to != NULL
+* 
+*/
 void gets_example_func(void) {
   char buf[BUFFER_MAX_SIZE];
  
-  if (fgets(buf, sizeof(buf), stdin) == NULL) {
-        return 1;
+  if (fgets(buf, sizeof(buf), stdin) != NULL) {
+    buf[strlen(buf) - 1] = '\0';
+    printf("%s", buf);
   }
-  buf[strlen(buf) - 1] = '\0';
 }
 
-const char *get_dirname(const char *pathname) {
-  char *slash;
+/*
+* STR30-C. Do not attempt to modify string literals
+* Adding memcpy to the process and the new parameters
+*/
+char *get_dirname(const char *pathname, char *dirname, size_t size) {
+  const char *slash;
   slash = strrchr(pathname, '/');
   if (slash) {
-    *slash = '\0'; /* Undefined behavior */
+    ptrdiff_t slash_idx = slash - pathname;
+    if ((size_t)slash_idx < size) {
+      memcpy(dirname, pathname, slash_idx);
+      dirname[slash_idx] = '\0';  
+      return dirname;
+    }
   }
-  return pathname;
+  return 0;
 }
  
 
+/*
+* MSC34-C: Do not use deprecated or obsolescent functions.
+* TR35-C: Do not copy data from an unbounded source to a fixed lengt harray
+* fgets instad get
+*/
 void get_y_or_n(void) {  
 	char response[8];
 
-	printf("Continue? [y] n: ");  
-	gets(response);
+	printf("Continue? [y] n: ");
+  fgets(response, sizeof(response), stdin);
 
 	if (response[0] == 'n') 
 		exit(0);  
@@ -64,20 +75,26 @@ int main(int argc, char *argv[])
     char array3[16];
     char array4[16];
     char array5 []  = "01234567890123456";
-    char *ptr_char  = "new string literal";
-    int size_array1 = strlen("аналитик");
-    int size_array2 = 100;
-    
-   // char analitic1[size_array1]="аналитик";
-   // char analitic2[size_array2]="аналитик";
-    char analitic3[100]="аналитик";
+    char ptr_char[]  = "new string literal";
+    int size_array1 = wcslen(L"аналитик"); // Changed strlen to wslen
+    wchar_t analitic1[]  = L"аналитик";
+    printf("%i\n", size_array1);
+    wprintf (L"You entered %u characters.\n",wcslen(analitic1));
 
-    puts(get_dirname(__FILE__));
+    char dirname[260];
+    if (get_dirname(__FILE__, dirname, sizeof(dirname))) {
+      puts(dirname);
+    }
 
-        
-    strcpy(key, argv[1]);  
-    strcat(key, " = ");  
-    strcat(key, argv[2]);
+    /*
+    * Changed deprecated function and adding Null-terminated byte
+    */
+    strncpy(key, argv[1], sizeof(key));  
+    key[sizeof(key)-1]='\0';
+    strncat(key, " = ", 2);
+    key[sizeof(key)-1]='\0';
+    strncat(key, argv[2], sizeof(key));
+    key[sizeof(key)-1]='\0';
 
 
     fgets(response,sizeof(response),stdin);
@@ -94,14 +111,18 @@ int main(int argc, char *argv[])
     puts (s2);
     printf ("\n");
     
-    strncpy(array3, array5, sizeof(array3));  
-    strncpy(array4, array3, strlen(array3));
+    strncpy(array3, array5, sizeof(array3));
+    /*
+    * strlen looks for the null-terminated byte and due the variable is not initialized it will not find it
+    * In addition, the size to take into account has to be that of the recepter, that is, array4
+    */  
+    strncpy(array4, array3, sizeof(array4));
     
     array5 [0] = 'M';
-    ptr_char [0] = 'N';
+    ptr_char [0] = 'N'; // Changed variable initialization
+    printf("%s\n", ptr_char);
     
     array3[sizeof(array3)-1]='\0';
-    
     
     return 0;
 }
